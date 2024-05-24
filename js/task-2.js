bbf052a9ad8fa310718e2a19f6c182a0793b2bd5
Bohdan Lytvynets <157913587+Bohdan-Lytvynets@users.noreply.github.com>
2024-05-24 19:57:52 +0200

    commit

---
+++ js/task-2.js
@@ -0,0 +1,46 @@
+const images = [
+  {
+    url: "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?dpr=2&h=750&w=1260",
+    alt: "White and Black Long Fur Cat",
+  },
+  {
+    url: "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?dpr=2&h=750&w=1260",
+    alt: "Orange and White Koi Fish Near Yellow Koi Fish",
+  },
+  {
+    url: "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?dpr=2&h=750&w=1260",
+    alt: "Group of Horses Running",
+  },
+  {
+    url: "https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg",
+    alt: "Alpine Spring Meadows",
+  },
+  {
+    url: "https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg",
+    alt: "Nature Landscape",
+  },
+  {
+    url: "https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg",
+    alt: "Lighthouse Coast Sea",
+  }
+];
+
+const list = document.querySelector(".gallery");
+
+function myImages(myImage) {
+  return `<li class="img-item">
+  <img class="js-img-item-img" src=${myImage.url} alt="${myImage.alt}"/>
+</li>`;
+}
+
+function imgTemplate(arr) {
+  return arr.map(myImages).join('');
+}
+
+const markup = imgTemplate(images.slice(0, 3));
+
+list.innerHTML = markup;
+
+const imgMy = document.querySelectorAll("js-img-item-img");
+imgMy.style.width = `360px`;
+
